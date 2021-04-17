c4abc79639633881102bb63e1b90d420f2d8d61f
Victor Kurinny <tyokman@gmail.com>
2021-04-17 13:12:32 +0300

    Сумма двух однозначных чисел

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -10,16 +10,46 @@ import XCTest
 
 class Calculator {
     func calculate(_ input: String) -> String? {
-        nil
+        let chars = Array(input)
+        guard chars.count == 3 else {
+            return nil
+        }
+
+        let digit1 = Int(String(chars[0]))!
+        let digit2 = Int(String(chars[2]))!
+        let result = digit1 + digit2
+
+        return "\(result)"
     }
 }
 
 class CalculatorTests: XCTestCase {
     func test_returnsError_forEmptyInput() {
+        expect(nil, for: "")
+    }
+
+    func test_sumOfTwoDigits() {
+        expect("3", for: "1+2")
+    }
+}
+
+extension CalculatorTests {
+    private func expect(
+        _ expectedResult: String?,
+        for input: String,
+        file: StaticString = #file,
+        line: UInt = #line
+    ) {
         let sut = Calculator()
 
-        let result = sut.calculate("")
+        let result = sut.calculate(input)
 
-        XCTAssertNil(result)
+        XCTAssertEqual(
+            result,
+            expectedResult,
+            "Expected \(String(describing: expectedResult)), got \(String(describing: result)) instead",
+            file: file,
+            line: line
+        )
     }
 }
