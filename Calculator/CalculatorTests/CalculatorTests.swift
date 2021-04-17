0044a1c1130dd1b7ca85701e87de41e27d9b493d
Victor Kurinny <tyokman@gmail.com>
2021-04-17 13:04:11 +0300

    Возвращаем ошибку если пользователь ничего не ввел

--- Calculator/CalculatorTests/CalculatorTests.swift
+++ Calculator/CalculatorTests/CalculatorTests.swift
@@ -8,26 +8,18 @@
 import XCTest
 @testable import Calculator
 
+class Calculator {
+    func calculate(_ input: String) -> String? {
+        nil
+    }
+}
+
 class CalculatorTests: XCTestCase {
+    func test_returnsError_forEmptyInput() {
+        let sut = Calculator()
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        let result = sut.calculate("")
+
+        XCTAssertNil(result)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
