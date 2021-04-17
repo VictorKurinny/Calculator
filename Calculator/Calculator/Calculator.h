c7492f34883c07fe1cd3adfd5a13a78f650c6fa3
Victor Kurinny <tyokman@gmail.com>
2021-04-17 12:48:51 +0300

    Добавлен новый macos таргет

---
+++ Calculator/Calculator/Calculator.h
@@ -0,0 +1,18 @@
+//
+//  Calculator.h
+//  Calculator
+//
+//  Created by Victor Kurinny on 17.04.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Calculator.
+FOUNDATION_EXPORT double CalculatorVersionNumber;
+
+//! Project version string for Calculator.
+FOUNDATION_EXPORT const unsigned char CalculatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Calculator/PublicHeader.h>
+
+
